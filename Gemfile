8462820559cd5b800f060fc5aac0f9ec771cfd3a
Benjamin Eycken <be.eycken@gmail.com>
2021-06-25 12:14:55 +0200

    respec and test done

--- Gemfile
+++ Gemfile
@@ -21,7 +21,6 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
 gem 'rspec', '~> 3.5'
 
 # Use Active Storage variant
@@ -33,6 +32,8 @@ gem 'bootsnap', '>= 1.4.4', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec', '~> 3.5'
+  gem 'factory_bot_rails'
 end
 
 group :development do
--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -1,7 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe "Games", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+describe "Games API", type: :request do
+  describe "GET /games" do
+    it 'return all Games' do
+      game1 = FactoryBot.create(:game, result: "Ben win")
+      FactoryBot.create(:move, name: "ben", move: "paper", game_id: game1.id )
+      FactoryBot.create(:move, name: "bot", move: "rock", game_id: game1.id )
+  
+      game2 = FactoryBot.create(:game, result: "Ben lose")
+      FactoryBot.create(:move, name: "ben", move: "paper", game_id: game1.id )
+      FactoryBot.create(:move, name: "bot", move: "scissor", game_id: game1.id )
+  
+      get '/api/v1/games'
+  
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
   end
+
+  describe "POST /games" do
+    it 'create a new games' do
+      expect{
+        post '/api/v1/games', params: {name: "ben", move: "paper"}
+    }.to change { Game.count}.from(0).to(1)
+
+      expect(response).to have_http_status(:created)
+    end
+  end
+  
+  
 end
